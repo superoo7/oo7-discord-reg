01cb24ea76bcdfb75a5cbcd576fe85fdc2b4946d
Lai Weng Han <jlwhoo7@gmail.com>
2018-02-20 08:23:31 +0800

    Added channel restriction

--- src/index.js
+++ src/index.js
@@ -9,10 +9,17 @@ import 'babel-polyfill';
 
 dotenv.config();
 
-import { getDateTimeFromTimestamp, timeConvertMessage } from './util';
+import {
+  getDateTimeFromTimestamp,
+  timeConvertMessage
+} from './util';
 
 // Controller
-import { registration, update, lastMessage } from './controller/user';
+import {
+  registration,
+  update,
+  lastMessage
+} from './controller/user';
 import displayUser from './controller/display_user';
 
 // Database
@@ -30,98 +37,109 @@ client.on('ready', () => {
 
 // on message receive
 client.on('message', msg => {
-  // GET INFO FOR MESSAGE
-  let {
-    id: currentMessageId,
-    author: { username: currentUsername, id: currentUserId },
-    content: currentContent,
-    createdTimestamp: currentCreatedTimestamp
-  } = msg;
+  if (msg.channel.id !== config.channelId) {
+    return;
+  } else {
+    // GET INFO FOR MESSAGE
+    let {
+      id: currentMessageId,
+      author: {
+        username: currentUsername,
+        id: currentUserId
+      },
+      content: currentContent,
+      createdTimestamp: currentCreatedTimestamp
+    } = msg;
 
-  if (currentContent.substring(0, 1) == config.trigger) {
-    var args = msg.content.substring(1).split(' ');
-    var cmd = args[0];
-    args = args.splice(1);
-    // CONSOLE LOGGING
-    logger.info(`CMD: ${cmd}`);
-    logger.info(`ARGS: ${args}`);
-    console.log(args);
-    // END
-    switch (cmd) {
-      case 'help':
-        msg.reply(`\n Stephard-Registration Bot v1.0 \n
+    if (currentContent.substring(0, 1) == config.trigger) {
+      var args = msg.content.substring(1).split(' ');
+      var cmd = args[0];
+      args = args.splice(1);
+      // CONSOLE LOGGING
+      logger.info(`CMD: ${cmd}`);
+      logger.info(`ARGS: ${args}`);
+      console.log(args);
+      // END
+      switch (cmd) {
+        case 'help':
+          msg.reply(`\n Stephard-Registration Bot v1.0 \n
 Type \`${
-          config.trigger
-        }reg <steem_name>\` to register your steem account with discord.\n
+            config.trigger
+          }reg <steem_name>\` to register your steem account with discord.\n
 Type \`${
-          config.trigger
-        }update <steem_name>\` to update your steem account name.\n
+            config.trigger
+          }update <steem_name>\` to update your steem account name.\n
 Type \`${config.trigger}last\` to check your last message.
           `);
-        break;
-      case 'reg':
-        if (args.length === 1) {
-          if (args[0].match(/^[a-z][a-z0-9\-\.]+$/)) {
-            let regMsg = async () => {
-              let discordMessage = await registration(
-                currentUsername,
-                currentUserId,
-                args[0]
+          break;
+        case 'reg':
+          if (args.length === 1) {
+            if (args[0].match(/^[a-z][a-z0-9\-\.]+$/)) {
+              let regMsg = async () => {
+                let discordMessage = await registration(
+                  currentUsername,
+                  currentUserId,
+                  args[0]
+                );
+                return discordMessage;
+              };
+              regMsg().then(res => {
+                console.log('discord msg: ', res);
+                msg.reply(res);
+              });
+            } else {
+              msg.reply(
+                'Your steem username format is wrong (should start with letter, all lowercase, only can contain lowercase letters, numbers and dash)'
               );
-              return discordMessage;
-            };
-            regMsg().then(res => {
+            }
+          } else {
+            msg.reply('Please follow the format');
+          }
+          break;
+        case 'update':
+          if (args.length === 1) {
+            if (args[0].match(/^[a-z][a-z0-9\-\.]+$/)) {
+              let updateMsg = async () => {
+                let discordMessage = await update(
+                  currentUsername,
+                  currentUserId,
+                  args[0]
+                );
+                return discordMessage;
+              };
+              updateMsg().then(res => {
+                console.log('discord msg: ', res);
+                msg.reply(res);
+              });
+            } else {
+              msg.reply(
+                'Your steem username format is wrong (should start with letter, all lowercase, only can contain lowercase letters, numbers and dash)'
+              );
+            }
+          } else {
+            msg.reply('Please follow the format');
+          }
+          break;
+        case 'last':
+          let lastMessageMsg = async () => {
+            let discordMessage = await lastMessage(
+              currentUserId
+            );
+            return discordMessage;
+          };
+          lastMessageMsg()
+            .then(res => {
               console.log('discord msg: ', res);
               msg.reply(res);
-            });
-          } else {
-            msg.reply(
-              'Your steem username format is wrong (should start with letter, all lowercase, only can contain lowercase letters, numbers and dash)'
-            );
-          }
-        } else {
-          msg.reply('Please follow the format');
-        }
-        break;
-      case 'update':
-        if (args.length === 1) {
-          if (args[0].match(/^[a-z][a-z0-9\-\.]+$/)) {
-            let updateMsg = async () => {
-              let discordMessage = await update(
-                currentUsername,
-                currentUserId,
-                args[0]
-              );
-              return discordMessage;
-            };
-            updateMsg().then(res => {
-              console.log('discord msg: ', res);
-              msg.reply(res);
-            });
-          } else {
-            msg.reply(
-              'Your steem username format is wrong (should start with letter, all lowercase, only can contain lowercase letters, numbers and dash)'
-            );
-          }
-        } else {
-          msg.reply('Please follow the format');
-        }
-        break;
-      case 'last':
-        let lastMessageMsg = async () => {
-          let discordMessage = await lastMessage(currentUserId);
-          return discordMessage;
-        };
-        lastMessageMsg()
-          .then(res => {
-            console.log('discord msg: ', res);
-            msg.reply(res);
-          })
-          .catch(err => console.log('ERR'));
-        break;
-      default:
-        msg.reply(`Type \`${config.trigger}help\` to get started`);
-        break;
+            })
+            .catch(err => console.log('ERR'));
+          break;
+        default:
+          msg.reply(
+            `Type \`${config.trigger}help\` to get started`
+          );
+          break;
+      }
     }
   }
 });
@@ -132,8 +150,13 @@ http
   .createServer(function(request, response) {
     displayUser().then(user => {
       console.log(user);
-      response.writeHead(200, { 'Content-Type': 'text/html' });
-      response.end(`<h1>superoo7 bot still alive!</h1>${user}}`, 'utf-8');
+      response.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      response.end(
+        `<h1>superoo7 bot still alive!</h1>${user}}`,
+        'utf-8'
+      );
     });
   })
   .listen(process.env.PORT || 5000);
